e459931c60738820ba7e537fb37ccd0cc066f70a
David Kern <david@mju.io>
2022-11-23 13:58:14 -0800

    start tokio runtime

--- hab-ve-mk3/src/main.rs
+++ hab-ve-mk3/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::runtime::Runtime;
+
+fn main() -> Result<()> {
+    let rt = Runtime::new()?;
+
+    rt.block_on(async {
+        pretty_env_logger::init();
+
+        log::debug!("exiting");
+        Ok(())
+    })
 }
