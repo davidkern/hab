a2972c873fc35df82fe98bc68593f23ab34384f3
David Kern <david@mju.io>
2022-11-23 19:06:02 -0800

    request and decode led status

--- hab-ve-mk3/src/mk3.rs
+++ hab-ve-mk3/src/mk3.rs
@@ -1,21 +1,26 @@
 use anyhow::Result;
 use bytes::{Buf, Bytes, BytesMut};
-use tokio_util::codec::{Decoder, FramedRead};
+use tokio_util::codec::{Decoder, Encoder, Framed};
 use tokio_stream::StreamExt;
 use std::num::Wrapping;
+use futures_util::sink::SinkExt;
 
 /// Store data from mk3 device into influxdb
 pub async fn run(path: &str) -> Result<()> {
     let builder = serial_io::build(path, 2400);
     let serial = serial_io::AsyncSerial::from_builder(&builder)?;
 
-    let decoder = VeMk3Decoder::default();
-    let mut frame_reader = FramedRead::new(serial, decoder);
+    let codec = VeMk3Codec::default();
+    let mut mk3 = Framed::new(serial, codec);
 
-    while let Some(result) = frame_reader.next().await {
+    while let Some(result) = mk3.next().await {
         match result {
             Ok(frame) => {
                 log::debug!("frame: {}", frame);
+                if let Frame::Version = frame {
+                    // request led status on each version frame
+                    mk3.send(RequestFrame::LedStatus).await;
+                }
             }
             Err(e) => {
                 log::error!("error: {}", e);
@@ -25,11 +30,11 @@ pub async fn run(path: &str) -> Result<()> {
     Ok(())
 }
 
-pub struct VeMk3Decoder {
+pub struct VeMk3Codec {
     synchronized: bool,
 }
 
-impl Default for VeMk3Decoder {
+impl Default for VeMk3Codec {
     fn default() -> Self {
         Self {
             synchronized: false,
@@ -40,20 +45,37 @@ impl Default for VeMk3Decoder {
 #[derive(Clone, Debug)]
 pub enum Frame {
     Version,
-    LedStatus,
+    LedStatus { led_status: LedStatus },
+}
+
+#[derive(Clone, Debug)]
+pub struct LedStatus {
+    mains: bool,
+    absorption: bool,
+    bulk: bool,
+    float: bool,
+    inverter: bool,
+    overload: bool,
+    low_battery: bool,
+    temperature: bool,
 }
 
 impl std::fmt::Display for Frame {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Version => { write!(f, "version") }
-            Self::LedStatus => { write!(f, "led") }
+            Self::LedStatus {
+                led_status
+            } => { write!(f, "led: {:?}", led_status) }
         }
     }
 }
 
-impl VeMk3Decoder {
+impl VeMk3Codec {
     fn decode_synchronized(&mut self, src: &mut BytesMut) -> Result<Option<Frame>> {
+        let buffer = Vec::from(&src[..]);
+        log::debug!("decode sync buffer: {:?}", buffer);
+
         if src.len() < 1 {
             log::debug!("decode sync, waiting for length byte");
             Ok(None)
@@ -68,6 +90,22 @@ impl VeMk3Decoder {
                     if src[1] == 0xff && src[2] == 0x56 {
                         // version frame
                         Ok(Some(Frame::Version))
+                    } else if src[1] == 0xff && src[2] == 0x4c {
+                        // led frame
+                        let active = src[3] | src[4]; // either on, or blinking
+
+                        Ok(Some(Frame::LedStatus {
+                            led_status: LedStatus {
+                                mains: active & 0x01 != 0,
+                                absorption: active & 0x02 != 0,
+                                bulk: active & 0x04 != 0,
+                                float: active & 0x08 != 0,
+                                inverter: active & 0x10 != 0,
+                                overload: active & 0x20 != 0,
+                                low_battery: active & 0x40 != 0,
+                                temperature: active & 0x80 != 0,    
+                            }
+                        }))
                     } else {
                         Ok(None)
                     }    
@@ -121,7 +159,7 @@ impl VeMk3Decoder {
     }
 }
 
-impl Decoder for VeMk3Decoder {
+impl Decoder for VeMk3Codec {
     type Item = Frame;
     type Error = anyhow::Error;
 
@@ -143,4 +181,35 @@ fn checksum_ok(src: &[u8]) -> bool {
     }
 
     checksum == Wrapping(0)
+}
+
+fn checksum(src: &[u8]) -> u8 {
+    let mut checksum: Wrapping<u8> = Wrapping(0);
+
+    for v in src.iter() {
+        checksum -= Wrapping(*v);
+    }
+
+    checksum.0
+}
+
+#[derive(Clone, Debug)]
+pub enum RequestFrame {
+    LedStatus,
+}
+
+impl Encoder<RequestFrame> for VeMk3Codec {
+    type Error = anyhow::Error;
+
+    fn encode(&mut self, item: RequestFrame, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        match item {
+            RequestFrame::LedStatus => {
+                let request = [0x02, 0xff, 0x4c, 0xb3];
+                dst.reserve(request.len());
+                dst.extend_from_slice(&request);
+            }
+        }
+
+        Ok(())
+    }
 }
